0d105bf56201910c30f7cb7bedecf8bfae451cd3
pez <emvenegas@gmail.com>
2021-02-06 23:49:25 +0000

    added markers to earthquake places

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 //var earthquakeData;
 var lastDayEarthquakes = [];
 
+var quakeIcon = L.icon({
+    iconUrl: 'quakeIcon.png',
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    popupAnchor: [0, -37],
+});
+
 var map = L.map('map').fitWorld();
 
 L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ypfODnzHuwl0bYyyvG3i', {
@@ -17,7 +24,14 @@ $.ajax({
 	success: function(result) {
         console.log('populate options' , result.earthquakeData.features[0]);
         for (var i = 0; i < result.earthquakeData.features.length; i++) {
-            
+            var quakePos = result.earthquakeData.features[i].geometry.coordinates;
+            var quakePopup = L.popup({
+                maxWidth: 400
+            }).setContent('On every stop sign we stop and wait for the rest to rejoin the group before we continue.');
+
+            L.marker([quakePos[1], quakePos[0]], {icon: quakeIcon}).addTo(map).bindPopup(quakePopup)
+
+
         }
     },
     error: function(jqXHR, textStatus, errorThrown) {
