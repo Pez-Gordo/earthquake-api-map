46dd8ea41f59afabf1b78d254f0383e255d53da0
pez <emvenegas@gmail.com>
2021-02-13 15:47:20 +0000

    updating earthquake and covid app

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var map = L.map('map').fitWorld();
 
-var bwMap = L.tileLayer('https://api.maptiler.com/maps/toner/{z}/{x}/{y}.png?key=ypfODnzHuwl0bYyyvG3i', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    crossOrigin: true
-}).addTo(map)
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicGV6IiwiYSI6ImNraWFlcDVsYTBpMW0ycnJreWRxdnNneXIifQ._2kq-bt8gs8Wmc5JIY-6NQ'
+}).addTo(map);
 
 map.setZoom(3);
 
